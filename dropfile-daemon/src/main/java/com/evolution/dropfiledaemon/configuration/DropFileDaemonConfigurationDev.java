727ed48a5de181bd8611734d73de72e36dea9ef5
Maksym Lukaretkyi <you@example.com>
2025-12-06 14:18:11 +0200

    cleanup config files

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/configuration/DropFileDaemonConfigurationDev.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/configuration/DropFileDaemonConfigurationDev.java
@@ -22,7 +22,7 @@ public class DropFileDaemonConfigurationDev {
 
     @Bean
     public DropFileAppConfig.DropFileDaemonAppConfig appConfig(
-            @Value("${dropfile.download.directory}") String downloadDirectory,
+            @Value("${dropfile.daemon.download.directory}") String downloadDirectory,
             @Value("${dropfile.daemon.port}") Integer daemonPort) {
         log.info("Provided download directory: {}", downloadDirectory);
         log.info("Provided daemon port: {}", daemonPort);
