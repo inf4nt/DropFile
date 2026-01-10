d640a0b2b5bbbd1d8dc4a4ad69bf412cadb6c8c3
Maksym Lukaretkyi <you@example.com>
2026-01-10 13:43:16 +0200

    added global exception handler for api calls

---
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/controller/ApiControllerExceptionHandler.java
@@ -0,0 +1,28 @@
+package com.evolution.dropfiledaemon.controller;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.util.ObjectUtils;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice(assignableTypes = {
+        ApiConnectionsAccessRestController.class,
+        ApiConnectionsShareRestController.class,
+        ApiConnectionsRestController.class,
+        ApiDaemonRestController.class,
+        ApiShareRestController.class
+})
+public class ApiControllerExceptionHandler {
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> exception(Exception e) {
+        if (ObjectUtils.isEmpty(e.getMessage())) {
+            return ResponseEntity.badRequest().body(
+                    e.getClass().getName()
+            );
+        }
+        return ResponseEntity.badRequest().body(
+                e.getClass().getName() + ". Message: " + e.getMessage()
+        );
+    }
+}
