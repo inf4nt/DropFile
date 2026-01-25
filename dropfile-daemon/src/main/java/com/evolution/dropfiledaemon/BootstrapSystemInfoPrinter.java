4201f42e71aea53fa841206c97621737392d73ce
Maksym Lukaretkyi <you@example.com>
2026-01-25 12:46:25 +0200

    Added daemon configuration to BootstrapSystemInfoPrinter

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/BootstrapSystemInfoPrinter.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/BootstrapSystemInfoPrinter.java
@@ -1,32 +1,46 @@
 package com.evolution.dropfiledaemon;
 
+import com.evolution.dropfile.store.app.AppConfigStore;
 import com.evolution.dropfiledaemon.system.SystemInfoProvider;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.context.event.ApplicationReadyEvent;
-import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.EventListener;
 import org.springframework.stereotype.Component;
 
 import java.util.Map;
 
 @Component
-public class BootstrapSystemInfoPrinter
-        implements ApplicationListener<ApplicationReadyEvent> {
+public class BootstrapSystemInfoPrinter {
 
     private final SystemInfoProvider systemInfoProvider;
 
+    private final AppConfigStore appConfigStore;
+
+    private final ObjectMapper objectMapper;
+
     @Autowired
-    public BootstrapSystemInfoPrinter(SystemInfoProvider systemInfoProvider) {
+    public BootstrapSystemInfoPrinter(SystemInfoProvider systemInfoProvider,
+                                      AppConfigStore appConfigStore,
+                                      ObjectMapper objectMapper) {
         this.systemInfoProvider = systemInfoProvider;
+        this.appConfigStore = appConfigStore;
+        this.objectMapper = objectMapper;
     }
 
-    @Override
-    public void onApplicationEvent(ApplicationReadyEvent event) {
+    @SneakyThrows
+    @EventListener(ApplicationReadyEvent.class)
+    public void onApplicationEvent() {
         Map<String, Object> systemInfo = systemInfoProvider.getSystemInfo();
-        for (Map.Entry<String, Object> entry : systemInfo.entrySet()) {
-            System.out.println("================================");
-            System.out.println(entry.getKey());
-            System.out.println(entry.getValue());
-        }
+        System.out.println("================================");
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(
+                systemInfo
+        ));
+        System.out.println("================================");
+        System.out.println(objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(
+                appConfigStore.getRequired().daemonAppConfig()
+        ));
         System.out.println("================================");
     }
 }
