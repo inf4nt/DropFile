fe46fc5d864a6d4da1bd09bca18029598c75065d
Maksym Lukaretkyi <you@example.com>
2026-02-18 15:30:49 +0200

    Added CLI -XX:ActiveProcessorCount=1 -Xmx128m -Xms128m Daemon -XX:ActiveProcessorCount=1 -Xmx256m -Xms256m

--- release/windows/dropfile-daemon.cmd
+++ release/windows/dropfile-daemon.cmd
@@ -1,3 +1,3 @@
 @echo off
 set DROPFILE_HOME=%~dp0..
-"%DROPFILE_HOME%\runtime\bin\java.exe" -jar "%DROPFILE_HOME%\dropfile-daemon\dropfile-daemon.jar" %*
+"%DROPFILE_HOME%\runtime\bin\java.exe" -XX:ActiveProcessorCount=1 -Xmx256m -Xms256m  -jar "%DROPFILE_HOME%\dropfile-daemon\dropfile-daemon.jar" %*
--- release/windows/dropfile.cmd
+++ release/windows/dropfile.cmd
@@ -1,3 +1,3 @@
 @echo off
 set DROPFILE_HOME=%~dp0..
-"%DROPFILE_HOME%\runtime\bin\java.exe" -jar "%DROPFILE_HOME%\dropfile-cli\dropfile-cli.jar" %*
+"%DROPFILE_HOME%\runtime\bin\java.exe" -XX:ActiveProcessorCount=1 -Xmx128m -Xms128m -jar "%DROPFILE_HOME%\dropfile-cli\dropfile-cli.jar" %*
