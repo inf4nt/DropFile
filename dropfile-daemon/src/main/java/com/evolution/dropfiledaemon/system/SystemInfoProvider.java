bfd40b45ad8aa75ee80ae586c6b864d85ff18911
Maksym Lukaretkyi <you@example.com>
2026-02-21 10:33:08 +0200

    Added PROCESS_ID to SystemInfoProvider

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/system/SystemInfoProvider.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/system/SystemInfoProvider.java
@@ -7,15 +7,19 @@ import org.springframework.stereotype.Component;
 import java.lang.management.ManagementFactory;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.UUID;
 
 @RequiredArgsConstructor
 @Component
 public class SystemInfoProvider {
 
+    private static final String PROCESS_ID = UUID.randomUUID().toString();
+
     private final FileHelper fileHelper;
 
     public Map<String, Object> getSystemInfo() {
         return new LinkedHashMap<>() {{
+            put("SystemInfoProvider.PROCESS_ID", PROCESS_ID);
             put("Runtime.getRuntime().availableProcessors()", Runtime.getRuntime().availableProcessors());
             put("ManagementFactory.getMemoryMXBean().getHeapMemoryUsage()", ManagementFactory.getMemoryMXBean().getHeapMemoryUsage());
             put("ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage()", ManagementFactory.getMemoryMXBean().getNonHeapMemoryUsage());
