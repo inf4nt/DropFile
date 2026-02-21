374def2b13ea557801e4c89e82bec131598b2849
Maksym Lukaretkyi <you@example.com>
2026-02-21 12:53:00 +0200

    Added logs for daemon removed WSL integration

--- release/windows/dropfile-daemon.cmd
+++ release/windows/dropfile-daemon.cmd
@@ -1,3 +1,7 @@
 @echo off
-set DROPFILE_HOME=%~dp0..
-"%DROPFILE_HOME%\runtime\bin\java.exe" -XX:ActiveProcessorCount=1 -Xmx256m -Xms256m -jar "%DROPFILE_HOME%\dropfile-daemon\dropfile-daemon.jar" %*
+
+set JAR_PATH=%DROPFILE_HOME%\dropfile-daemon\dropfile-daemon.jar
+set JAVA_PATH=%DROPFILE_HOME%\runtime\bin\java.exe
+set LOG_PATH=%DROPFILE_HOME%\logs
+
+"%JAVA_PATH%" "-Dlogging.path=%LOG_PATH%" -XX:ActiveProcessorCount=1 -Xmx256m -Xms256m -jar "%JAR_PATH%" %*
--- release/windows/dropfile.cmd
+++ release/windows/dropfile.cmd
@@ -1,3 +1,6 @@
 @echo off
-set DROPFILE_HOME=%~dp0..
-"%DROPFILE_HOME%\runtime\bin\java.exe" -XX:ActiveProcessorCount=1 -Xmx128m -Xms128m -jar "%DROPFILE_HOME%\dropfile-cli\dropfile-cli.jar" %*
+
+set JAR_PATH=%DROPFILE_HOME%\dropfile-cli\dropfile-cli.jar
+set JAVA_PATH=%DROPFILE_HOME%\runtime\bin\java.exe
+
+"%JAVA_PATH%" -XX:ActiveProcessorCount=1 -Xmx128m -Xms128m -jar "%JAR_PATH%" %*
