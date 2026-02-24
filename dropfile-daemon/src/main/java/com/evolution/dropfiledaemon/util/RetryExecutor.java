420e955e70e4b6a063a16ec06858164012430ad2
Maksym Lukaretkyi <you@example.com>
2026-02-24 17:28:49 +0200

    Removed unused fallbackMapper in Retry

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/util/RetryExecutor.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/util/RetryExecutor.java
@@ -1,6 +1,5 @@
 package com.evolution.dropfiledaemon.util;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
@@ -11,15 +10,12 @@ import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.Callable;
 import java.util.function.BiConsumer;
-import java.util.function.Function;
 import java.util.function.Predicate;
 
-@AllArgsConstructor
+@RequiredArgsConstructor
 public class RetryExecutor<T> {
 
     private final Callable<T> callable;
-
-    private final Function<List<Exception>, T> fallbackMapper;
 
     private final BiConsumer<Integer, Exception> doOnError;
 
@@ -67,9 +63,6 @@ public class RetryExecutor<T> {
             currentAttempt++;
             Thread.sleep(delay.toMillis());
         }
-        if (fallbackMapper != null) {
-            return fallbackMapper.apply(exceptions);
-        }
         throw new RetryExecutorException(exceptions);
     }
 
@@ -81,8 +74,6 @@ public class RetryExecutor<T> {
     public static class RetryExecutorBuilder<T> {
 
         private final Callable<T> callable;
-
-        private Function<List<Exception>, T> fallbackMapper;
 
         private BiConsumer<Integer, Exception> doOnError;
 
@@ -117,11 +108,6 @@ public class RetryExecutor<T> {
             return this;
         }
 
-        public RetryExecutorBuilder<T> fallbackMapper(Function<List<Exception>, T> fallbackMapper) {
-            this.fallbackMapper = fallbackMapper;
-            return this;
-        }
-
         public RetryExecutorBuilder<T> retryIf(Predicate<RetryIfContainer<T>> retryIf) {
             this.retryIf = Objects.requireNonNull(retryIf);
             return this;
@@ -130,7 +116,6 @@ public class RetryExecutor<T> {
         public RetryExecutor<T> build() {
             return new RetryExecutor<>(
                     callable,
-                    fallbackMapper,
                     doOnError,
                     doOnSuccessful,
                     retryIf,
