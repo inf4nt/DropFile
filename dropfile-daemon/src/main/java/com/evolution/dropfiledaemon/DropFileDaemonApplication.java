95cae3321bd863e64699f0a74505ec59cb1dfa54
Maksym Lukaretkyi <you@example.com>
2026-03-06 13:45:14 +0200

    shutdown refactoring

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/DropFileDaemonApplication.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/DropFileDaemonApplication.java
@@ -3,11 +3,18 @@ package com.evolution.dropfiledaemon;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.security.autoconfigure.UserDetailsServiceAutoConfiguration;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class DropFileDaemonApplication {
 
+    private static ConfigurableApplicationContext context;
+
     public static void main(String[] args) {
-        SpringApplication.run(DropFileDaemonApplication.class, args);
+        context = SpringApplication.run(DropFileDaemonApplication.class, args);
+    }
+
+    public static void exit() {
+        Thread.ofVirtual().start(() -> SpringApplication.exit(context, () -> 0));
     }
 }
--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/facade/ApiDaemonFacade.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/facade/ApiDaemonFacade.java
@@ -1,16 +1,11 @@
 package com.evolution.dropfiledaemon.facade;
 
 import com.evolution.dropfile.common.dto.DaemonInfoResponseDTO;
+import com.evolution.dropfiledaemon.DropFileDaemonApplication;
 import com.evolution.dropfiledaemon.configuration.ApplicationConfigStore;
 import com.evolution.dropfiledaemon.system.SystemInfoProvider;
 import lombok.RequiredArgsConstructor;
-import lombok.SneakyThrows;
-import org.springframework.boot.SpringApplication;
-import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
-
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 
 @RequiredArgsConstructor
 @Component
@@ -20,20 +15,15 @@ public class ApiDaemonFacade {
 
     private final SystemInfoProvider systemInfoProvider;
 
-    private final ApplicationContext applicationContext;
-
-    public void shutdown() {
-        try (ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor()) {
-            executorService.execute(() -> SpringApplication.exit(applicationContext, () -> 0));
-        }
-    }
-
-    @SneakyThrows
     public DaemonInfoResponseDTO info() {
         return new DaemonInfoResponseDTO(
                 systemInfoProvider.getSystemInfo(),
                 applicationConfigStore.getDaemonAppConfigStore().getRequired()
         );
+    }
+
+    public void shutdown() {
+        DropFileDaemonApplication.exit();
     }
 
     public void cacheReset() {
