3586d12a2b533f5c7967e699a4f5a5979adc913e
Maksym Lukaretskyi <noemail@example.com>
2026-03-19 15:14:39 +0200

    Added canTransitionTo validation to FileDownloadEntryStore

--- dropfile-store/src/main/java/com/evolution/dropfile/store/download/FileDownloadEntryStore.java
+++ dropfile-store/src/main/java/com/evolution/dropfile/store/download/FileDownloadEntryStore.java
@@ -4,4 +4,25 @@ import com.evolution.dropfile.store.framework.KeyValueStore;
 
 public interface FileDownloadEntryStore
         extends KeyValueStore<DownloadFileEntry> {
+
+    @Override
+    default void validate(String key, DownloadFileEntry value) {
+        DownloadFileEntry downloadFileEntry = get(key)
+                .map(it -> it.getValue())
+                .orElse(null);
+        if (downloadFileEntry == null) {
+            return;
+        }
+        DownloadFileEntry.DownloadFileEntryStatus currentStatus = downloadFileEntry.status();
+        if (!canTransitionTo(currentStatus)) {
+            throw new IllegalStateException(String.format("Status transition failed. Key %s status from %s to %s", key, currentStatus, value.status()));
+        }
+    }
+
+    private boolean canTransitionTo(DownloadFileEntry.DownloadFileEntryStatus current) {
+        return switch (current) {
+            case DOWNLOADING -> true;
+            case COMPLETED, ERROR, INTERRUPTED, STOPPED -> false;
+        };
+    }
 }
