0a9c3422db5b57b4a370c689241ce27f3007ea5c
Maksym Lukaretskyi <noemail@example.com>
2026-03-20 12:11:21 +0200

    Added addShutdownHook and Interrupt

--- dropfile-cli/src/main/java/com/evolution/dropfilecli/DropFileCliCommandLineRunner.java
+++ dropfile-cli/src/main/java/com/evolution/dropfilecli/DropFileCliCommandLineRunner.java
@@ -1,11 +1,16 @@
 package com.evolution.dropfilecli;
 
+import jakarta.annotation.PostConstruct;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.ApplicationContext;
 import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 import picocli.spring.PicocliSpringFactory;
+
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 @RequiredArgsConstructor
 @Component
@@ -15,10 +20,20 @@ public class DropFileCliCommandLineRunner implements CommandLineRunner {
 
     private final RootCommand root;
 
+    private final ExecutorService executorService = Executors.newVirtualThreadPerTaskExecutor();
+
+    @PostConstruct
+    public void postConstruct() {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            executorService.shutdownNow();
+        }));
+    }
+
     @Override
     public void run(String... args) {
-        CommandLine commandLine = new CommandLine(root, new PicocliSpringFactory(applicationContext));
-        commandLine.setUnmatchedArgumentsAllowed(true);
+        CompletableFuture.runAsync(() -> {
+            CommandLine commandLine = new CommandLine(root, new PicocliSpringFactory(applicationContext));
+            commandLine.setUnmatchedArgumentsAllowed(true);
 //        commandLine.setExecutionExceptionHandler(new CommandLine.IExecutionExceptionHandler() {
 //            @Override
 //            public int handleExecutionException(Exception ex, CommandLine commandLine, CommandLine.ParseResult fullParseResult) throws Exception {
@@ -26,7 +41,8 @@ public class DropFileCliCommandLineRunner implements CommandLineRunner {
 //                return 0;
 //            }
 //        });
-        commandLine.execute(args);
-        System.exit(0);
+            commandLine.execute(args);
+            System.exit(0);
+        }, executorService).join();
     }
 }
