c3d44c57ad86d56e9948e2f06e425f4ca20b15aa
Maksym Lukaretskyi <noemail@example.com>
2026-06-02 19:34:23 +0300

    added ApplicationFingerprintSupplierInitializationValidationProcedure

---
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/configuration/ApplicationFingerprintSupplierInitializationValidationProcedure.java
@@ -0,0 +1,18 @@
+package com.evolution.dropfiledaemon.configuration;
+
+import com.evolution.dropfile.store.framework.file.ApplicationFingerprintSupplier;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.util.ObjectUtils;
+
+@Component
+public class ApplicationFingerprintSupplierInitializationValidationProcedure {
+
+    @Autowired
+    public void init(ApplicationFingerprintSupplier supplier) {
+        String key = supplier.get();
+        if (ObjectUtils.isEmpty(key)) {
+            throw new RuntimeException("ApplicationFingerprintSupplier key must not be empty");
+        }
+    }
+}
