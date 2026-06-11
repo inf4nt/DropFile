c1a762311c09e6a358a65d9c94a516f7e32a0f61
Maksym Lukaretskyi <noemail@example.com>
2026-06-11 21:36:15 +0300

    Changed memory usage to -Xmx128m -Xms64m

--- release/windows/dropfile-daemon.cmd
+++ release/windows/dropfile-daemon.cmd
@@ -7,4 +7,4 @@ set JAVA_PATH=%DROPFILE_HOME%\runtime\bin\java.exe
 set LOG_PATH=%DROPFILE_HOME%\logs
 set SPRING_APPLICATION_PROPERTIES_PATH=%DROPFILE_HOME%\conf\dropfile-daemon.application.properties
 
-"%JAVA_PATH%" "-Dlogging.path=%LOG_PATH%" -XX:ActiveProcessorCount=1 -Xmx256m -Xms256m -jar "%JAR_PATH%" "--spring.config.location=file:%SPRING_APPLICATION_PROPERTIES_PATH%" %*
+"%JAVA_PATH%" "-Dlogging.path=%LOG_PATH%" -XX:ActiveProcessorCount=1 -Xmx128m -Xms64m -jar "%JAR_PATH%" "--spring.config.location=file:%SPRING_APPLICATION_PROPERTIES_PATH%" %*
