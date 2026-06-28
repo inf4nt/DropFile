dfccf03718f2e5ff700e212d7f818596e2a0fb88
Maksym Lukaretskyi <noemail@example.com>
2026-06-28 11:06:58 +0300

    Added logging to InstallationSeedBootstrapStoreInitializationProcedure

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/bootstrap/procedure/InstallationSeedBootstrapStoreInitializationProcedure.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/bootstrap/procedure/InstallationSeedBootstrapStoreInitializationProcedure.java
@@ -3,12 +3,14 @@ package com.evolution.dropfiledaemon.bootstrap.procedure;
 import com.evolution.dropfile.store.framework.bootstrap.BootstrapStoreInitializationProcedure;
 import com.evolution.dropfile.store.seed.InstallationSeedBootstrapStore;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Profile;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
 
+@Slf4j
 @Profile("prod")
 @Component
 @RequiredArgsConstructor
@@ -21,6 +23,7 @@ public class InstallationSeedBootstrapStoreInitializationProcedure
     public void init() {
         Optional<UUID> seed = store.get();
         if (seed.isEmpty()) {
+            log.info("No seed found. Initialization in progress");
             store.save(UUID.randomUUID());
         }
     }
