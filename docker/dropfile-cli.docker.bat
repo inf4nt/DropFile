184c7d56967cebe6c42a5ff01b9ff46cd0451301
Maksym Lukaretskyi <noemail@example.com>
2026-07-01 15:07:06 +0300

    fix

--- docker/dropfile-cli.docker.bat
+++
@@ -1,8 +0,0 @@
-@echo off
-docker run --rm ^
-  -v dropfile-daemon-conf:/daemon-conf ^
-  -e DROPFILE_DAEMON_HOST=host.docker.internal ^
-  -e DROPFILE_DAEMON_PORT=28282 ^
-  -e DROPFILE_DAEMON_DAEMON-SECRETS_DIRECTORY=/daemon-conf ^
-  -e DROPFILE_DAEMON_INSTALLATION-SEED_DIRECTORY=/daemon-conf ^
-  dropfile-cli %*
