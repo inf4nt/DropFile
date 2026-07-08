849542b7bea2af031ed371aee309e4a640b3161c
Maksym Lukaretskyi <noemail@example.com>
2026-07-08 14:26:41 +0300

    refactoring release dropfile and dropfile-daemon scripts

--- release/windows/dropfile-daemon.cmd
+++ release/windows/dropfile-daemon.cmd
@@ -1,16 +1,22 @@
 @echo off
 
-cd /d "%DROPFILE_HOME%"
+set "BIN_DIR=%~dp0"
+for %%I in ("%BIN_DIR%..") do set "APPLICATION_HOME=%%~fI"
 
-set JAR_PATH=%DROPFILE_HOME%\jars\dropfile-daemon.jar
-set JAVA_PATH=%DROPFILE_HOME%\runtime\bin\java.exe
-set LOG_PATH=%DROPFILE_HOME%\logs
-set SPRING_APPLICATION_PROPERTIES_PATH=%DROPFILE_HOME%\conf\dropfile-daemon.application.properties
+set "JAR_PATH=%APPLICATION_HOME%\jars\dropfile-daemon.jar"
+set "JAVA_PATH=%APPLICATION_HOME%\runtime\bin\java.exe"
+set "LOG_PATH=%APPLICATION_HOME%\logs"
+set "SPRING_APPLICATION_PROPERTIES_PATH=%APPLICATION_HOME%\conf\dropfile-daemon.application.properties"
+
+IF NOT DEFINED DROPFILE_DAEMON_APPLICATION_HOME_DIRECTORY (
+    SET "DROPFILE_DAEMON_APPLICATION_HOME_DIRECTORY=%APPLICATION_HOME%"
+)
 
 "%JAVA_PATH%" ^
-        "-Dlogging.path=%LOG_PATH%" ^
+         -Dlogging.path="%LOG_PATH%" ^
          -XX:ActiveProcessorCount=1 ^
          -Xmx256m -Xms64m ^
          -jar "%JAR_PATH%" ^
          "--spring.config.location=file:%SPRING_APPLICATION_PROPERTIES_PATH%" ^
+         "--dropfile.daemon.application-home.directory=%DROPFILE_DAEMON_APPLICATION_HOME_DIRECTORY%" ^
          %*
--- release/windows/dropfile.cmd
+++ release/windows/dropfile.cmd
@@ -1,14 +1,25 @@
 @echo off
 
-cd /d "%DROPFILE_HOME%"
+set "BIN_DIR=%~dp0"
+for %%I in ("%BIN_DIR%..") do set "APPLICATION_HOME=%%~fI"
 
-set JAR_PATH=%DROPFILE_HOME%\jars\dropfile-cli.jar
-set JAVA_PATH=%DROPFILE_HOME%\runtime\bin\java.exe
-set SPRING_APPLICATION_PROPERTIES_PATH=%DROPFILE_HOME%\conf\dropfile-cli.application.properties
+set "JAR_PATH=%APPLICATION_HOME%\jars\dropfile-cli.jar"
+set "JAVA_PATH=%APPLICATION_HOME%\runtime\bin\java.exe"
+set "SPRING_APPLICATION_PROPERTIES_PATH=%APPLICATION_HOME%\conf\dropfile-cli.application.properties"
+
+IF NOT DEFINED DROPFILE_DAEMON_DAEMON_SECRETS_DIRECTORY (
+    SET "DROPFILE_DAEMON_DAEMON_SECRETS_DIRECTORY=%APPLICATION_HOME%\conf"
+)
+
+IF NOT DEFINED DROPFILE_DAEMON_INSTALLATION_SEED_DIRECTORY (
+    SET "DROPFILE_DAEMON_INSTALLATION_SEED_DIRECTORY=%APPLICATION_HOME%\conf"
+)
 
 "%JAVA_PATH%" ^
         -XX:ActiveProcessorCount=1 ^
         -Xmx128m -Xms64m ^
         -jar "%JAR_PATH%" ^
         "--spring.config.location=file:%SPRING_APPLICATION_PROPERTIES_PATH%" ^
+        "--dropfile.daemon.daemon-secrets.directory=%DROPFILE_DAEMON_DAEMON_SECRETS_DIRECTORY%" ^
+        "--dropfile.daemon.installation-seed.directory=%DROPFILE_DAEMON_INSTALLATION_SEED_DIRECTORY%" ^
         %*
