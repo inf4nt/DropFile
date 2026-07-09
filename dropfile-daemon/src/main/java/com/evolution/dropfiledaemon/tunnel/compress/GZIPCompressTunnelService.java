99528b6e97ed9b71c50bb9ddcc777210d7a1b42f
Maksym Lukaretskyi <noemail@example.com>
2026-07-09 12:01:15 +0300

    Removed com.github.luben.zstd-jni GZIP as tunnel compress

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/tunnel/compress/GZIPCompressTunnelService.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/tunnel/compress/GZIPCompressTunnelService.java
@@ -1,29 +1,30 @@
 package com.evolution.dropfiledaemon.tunnel.compress;
 
 import com.evolution.dropfiledaemon.configuration.DaemonApplicationProperties;
-import com.github.luben.zstd.ZstdInputStream;
-import com.github.luben.zstd.ZstdOutputStream;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
+import java.util.zip.GZIPInputStream;
+import java.util.zip.GZIPOutputStream;
 
-@Component
 @RequiredArgsConstructor
-public class ZstdCompressTunnelService
-        implements CompressTunnelService {
+@Component
+public class GZIPCompressTunnelService implements CompressTunnelService {
 
     private final DaemonApplicationProperties daemonApplicationProperties;
 
     @Override
     public OutputStream compressWrapper(OutputStream outputStream) throws IOException {
-        return new ZstdOutputStream(outputStream, daemonApplicationProperties.daemonTunnelServerCompressLevel);
+        return new GZIPOutputStream(outputStream) {{
+            def.setLevel(daemonApplicationProperties.daemonTunnelServerCompressLevel);
+        }};
     }
 
     @Override
     public InputStream decompress(InputStream inputStream) throws IOException {
-        return new ZstdInputStream(inputStream);
+        return new GZIPInputStream(inputStream);
     }
 }
