3ef1427151f5ae0f5fa03b2c95a1c04458204c93
Maksym Lukaretskyi <noemail@example.com>
2026-08-25 13:03:28 +0300

    root command refactoring

--- dropfile-cli/src/main/java/com/evolution/dropfilecli/picocli/DropFileCliCommandLineRunner.java
+++ dropfile-cli/src/main/java/com/evolution/dropfilecli/picocli/DropFileCliCommandLineRunner.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 import picocli.spring.PicocliSpringFactory;
 
+import java.io.PrintWriter;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
@@ -31,43 +32,96 @@ public class DropFileCliCommandLineRunner implements CommandLineRunner {
         CompletableFuture.runAsync(() -> {
             CommandLine commandLine = new CommandLine(root, new PicocliSpringFactory(applicationContext));
             commandLine.setCommandName("dropfile");
-//            commandLine.setUnmatchedArgumentsAllowed(true);
 
-            CommandLine.IParameterExceptionHandler defaultHandler = commandLine.getParameterExceptionHandler();
+            addParameterExceptionHandler(commandLine);
+            addExecutionExceptionHandler(commandLine);
 
-            commandLine.setParameterExceptionHandler((ex, arr) -> {
-                Spinner.stop();
-                return defaultHandler.handleParseException(ex, arr);
-            });
+            addGlobalOptions(commandLine);
 
-            CommandLine.Model.OptionSpec liveOption = CommandLine.Model.OptionSpec
-                    .builder("--live")
-                    .type(boolean.class)
-                    .description("Run this command in live update mode")
-                    .scopeType(CommandLine.ScopeType.INHERIT)
-                    .build();
-            commandLine.getCommandSpec().addOption(liveOption);
-
-            CommandLine.Model.OptionSpec ignoreErrorOption = CommandLine.Model.OptionSpec
-                    .builder("--ignore-error")
-                    .type(boolean.class)
-                    .description("Continue polling even if the command encounters an error")
-                    .scopeType(CommandLine.ScopeType.INHERIT)
-                    .build();
-            commandLine.getCommandSpec().addOption(ignoreErrorOption);
+            hideInheritedOptionsFromHelp(commandLine);
 
             commandLine.setExecutionStrategy(liveExecutionStrategy);
 
-//            commandLine.setExecutionExceptionHandler(new CommandLine.IExecutionExceptionHandler() {
-//                @Override
-//                public int handleExecutionException(Exception ex, CommandLine commandLine, CommandLine.ParseResult fullParseResult) throws Exception {
-//                    PrintWriter err = commandLine.getErr();
-//                    err.println(ex.getMessage());
-//                    return 1;
-//                }
-//            });
+
             int execute = commandLine.execute(args);
             DropFileCliApplication.exit(execute);
         }, executorService).join();
+    }
+
+    private void addParameterExceptionHandler(CommandLine commandLine) {
+        CommandLine.IParameterExceptionHandler defaultHandler = commandLine.getParameterExceptionHandler();
+        commandLine.setParameterExceptionHandler((ex, arr) -> {
+            Spinner.stop();
+            return defaultHandler.handleParseException(ex, arr);
+        });
+
+    }
+
+    private void addExecutionExceptionHandler(CommandLine commandLine) {
+        commandLine.setExecutionExceptionHandler(new CommandLine.IExecutionExceptionHandler() {
+            @Override
+            public int handleExecutionException(Exception ex, CommandLine commandLine, CommandLine.ParseResult fullParseResult) throws Exception {
+                Spinner.stop();
+                if (isStacktraceEnabled(fullParseResult)) {
+                    ex.printStackTrace(commandLine.getErr());
+                    return 1;
+                }
+
+                PrintWriter err = commandLine.getErr();
+                err.println(ex.getMessage());
+                return 1;
+            }
+
+            private boolean isStacktraceEnabled(CommandLine.ParseResult parseResult) {
+                if (parseResult == null) {
+                    return false;
+                }
+                return parseResult.asCommandLineList().stream()
+                        .anyMatch(cmd -> cmd.getParseResult().hasMatchedOption("stacktrace"));
+            }
+        });
+    }
+
+    private void addGlobalOptions(CommandLine commandLine) {
+        CommandLine.Model.CommandSpec rootSpec = commandLine.getCommandSpec();
+
+        rootSpec.addOption(CommandLine.Model.OptionSpec.builder("--live")
+                .type(boolean.class)
+                .description("Run this command in live update mode")
+                .scopeType(CommandLine.ScopeType.INHERIT)
+                .build());
+
+        rootSpec.addOption(CommandLine.Model.OptionSpec.builder("--ignore-error")
+                .type(boolean.class)
+                .description("Continue polling even if the command encounters an error")
+                .scopeType(CommandLine.ScopeType.INHERIT)
+                .build());
+
+        rootSpec.addOption(CommandLine.Model.OptionSpec.builder("--stacktrace")
+                .type(boolean.class)
+                .description("Show detailed information about error")
+                .scopeType(CommandLine.ScopeType.INHERIT)
+                .build());
+    }
+
+    public static void hideInheritedOptionsFromHelp(CommandLine commandLine) {
+//        commandLine.getHelpSectionMap().put(
+//                CommandLine.Model.UsageMessageSpec.SECTION_KEY_OPTION_LIST,
+//                help -> {
+//                    CommandLine.Help.Layout layout = help.createDefaultLayout();
+//
+//                    for (CommandLine.Model.OptionSpec opt : help.commandSpec().options()) {
+//                        if (!opt.inherited() && !opt.hidden()) {
+//                            layout.addOption(opt, help.parameterLabelRenderer());
+//                        }
+//                    }
+//
+//                    return layout.toString();
+//                }
+//        );
+//
+//        for (CommandLine sub : commandLine.getSubcommands().values()) {
+//            hideInheritedOptionsFromHelp(sub);
+//        }
     }
 }
