48c28d2ba64c49938d3900310eeea5b8c3e12e7b
Maksym Lukaretskyi <your.email@example.com>
2026-08-26 22:05:56 +0300

    refactoring

--- dropfile-common/src/main/java/com/evolution/dropfile/common/dto/ApiQuickShareAddRequestDTO.java
+++ dropfile-common/src/main/java/com/evolution/dropfile/common/dto/ApiQuickShareAddRequestDTO.java
@@ -1,6 +1,6 @@
 package com.evolution.dropfile.common.dto;
 
-public record ApiQuickShareAddRequestDTO(String resource,
+public record ApiQuickShareAddRequestDTO(String resourcePath,
                                          boolean singleUse,
                                          boolean secure,
                                          String secret) {
