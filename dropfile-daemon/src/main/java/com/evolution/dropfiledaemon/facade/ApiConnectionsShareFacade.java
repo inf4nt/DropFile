63f12cc23b916af0a2c31e3ee1857889006c123a
Maksym Lukaretskyi <your.email@example.com>
2026-09-11 11:10:50 +0300

    Added timeout to connections share add

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/facade/ApiConnectionsShareFacade.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/facade/ApiConnectionsShareFacade.java
@@ -71,7 +71,7 @@ public class ApiConnectionsShareFacade {
                     );
                 }, value -> {
                     long timeoutMillis = getTimeout(timeout);
-                    log.info("Calculating sha256 file {} alias {} timeout {}", realPath, alias, t);
+                    log.info("Calculating sha256 file {} alias {} timeout {}", realPath, alias, timeoutMillis);
                     String sha256 = calculateSha256(realPath, timeoutMillis);
                     log.info("Calculating sha256 file {} alias {} finished {}", realPath, alias, sha256);
                     return value.withHash(sha256).withAccessible(true);
