8b0cb3ec3b15417084b2c80f1533a3de4523646b
Maksym Lukaretskyi <your.email@example.com>
2026-09-11 13:47:26 +0300

    Added WARNING message to connections download kill command

--- dropfile-cli/src/main/java/com/evolution/dropfilecli/command/connections/download/DownloadKillCommand.java
+++ dropfile-cli/src/main/java/com/evolution/dropfilecli/command/connections/download/DownloadKillCommand.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Component;
 import picocli.CommandLine;
 
 import java.net.http.HttpResponse;
+import java.util.Scanner;
 import java.util.Set;
 
 @Component
@@ -24,8 +25,33 @@ public class DownloadKillCommand extends AbstractApiBatchOperationResultCommandH
     @CommandLine.Parameters(index = "0..*", arity = "1..*", split = ",", description = "Download operation ids")
     private Set<String> operationIdCriteria;
 
+    @CommandLine.Option(names = {"-y"}, description = "Automatic yes to prompts")
+    private boolean assumeYes;
+
     @Override
     public HttpResponse<byte[]> execute() throws Exception {
         return daemonClient.connectionsDownloadKill(CriteriaEnvelope.map(operationIdCriteria));
+    }
+
+    @Override
+    public void run() {
+        if (assumeYes || runConfirmation()) {
+            System.out.println("Confirmed. Executing");
+            super.run();
+        } else {
+            System.out.println("Rejected");
+        }
+    }
+    private boolean runConfirmation() {
+        System.out.println("WARNING: This operation will permanently delete the file(s) from the filesystem.");
+        System.out.print("Enter 'y' to continue... ");
+
+        Scanner scanner = new Scanner(System.in);
+        if (!scanner.hasNextLine()) {
+            return false;
+        }
+
+        String input = scanner.nextLine().trim();
+        return input.equalsIgnoreCase("y") || input.equalsIgnoreCase("yes");
     }
 }
