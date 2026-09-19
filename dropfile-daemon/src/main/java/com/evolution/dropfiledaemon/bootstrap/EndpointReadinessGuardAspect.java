4d62848078cc47398d8c4676d83a1444ebb567b6
Maksym Lukaretskyi <your.email@example.com>
2026-09-19 10:36:28 +0300

    Fixed EndpointReadinessGuardAspect

--- dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/bootstrap/EndpointReadinessGuardAspect.java
+++ dropfile-daemon/src/main/java/com/evolution/dropfiledaemon/bootstrap/EndpointReadinessGuardAspect.java
@@ -21,7 +21,7 @@ public class EndpointReadinessGuardAspect {
 
     private volatile boolean ready = false;
 
-    @EventListener(DropFileDaemonBeforeApplicationReadyEvent.class)
+    @EventListener(DropFileDaemonApplicationReadyEvent.class)
     public void onApplicationReady() {
         log.info("DropFileDaemon is ready. Unlocking HTTP endpoints");
         this.ready = true;
@@ -34,19 +34,20 @@ public class EndpointReadinessGuardAspect {
 
     @Around("restControllers()")
     public Object checkReadiness(ProceedingJoinPoint pjp) throws Throwable {
-        if (!ready) {
-            log.info("Blocking incoming HTTP request: Application is not ready yet");
-            RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
-            if (requestAttributes instanceof ServletRequestAttributes servletRequestAttributes) {
-                HttpServletResponse response = servletRequestAttributes.getResponse();
-                if (response != null) {
-                    response.setStatus(HttpServletResponse.SC_NOT_FOUND);
-                    return null;
-                }
-            }
-            // fallback if there is no response
-            throw new ResponseStatusException(HttpStatus.NOT_FOUND);
+        if (ready) {
+            return pjp.proceed();
         }
-        return pjp.proceed();
+
+        log.info("Blocking incoming HTTP request: Application is not ready yet");
+        RequestAttributes requestAttributes = RequestContextHolder.getRequestAttributes();
+        if (requestAttributes instanceof ServletRequestAttributes servletRequestAttributes) {
+            HttpServletResponse response = servletRequestAttributes.getResponse();
+            if (response != null) {
+                response.setStatus(HttpServletResponse.SC_NOT_FOUND);
+                return null;
+            }
+        }
+        // fallback if there is no response
+        throw new ResponseStatusException(HttpStatus.NOT_FOUND);
     }
 }
